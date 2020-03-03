a3b4ef779d13c24134a3c3d12b5d45bb609407fe
Felipe <felipe_toffel@hotmail.com>
2020-03-03 10:28:06 -0300

    listar raças, sub raças incompleto -

--- dogCEOIOS/ViewController.swift
+++ dogCEOIOS/ViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import os.log
+
 
 class ViewController: UIViewController {
 
+    var racas :[String] = []
+    var listType = "raca"
+    
+    
+    @IBOutlet var TableView: UIView!
+    
+    @IBOutlet weak var imagemview: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    func urltodasasracas(_sender: AnyObject){
+        let url = URL(string: "http://dog.ceo/api/breeds/list/all")!
+        if UIApplication.shared.canOpenURL(url){
+        UIApplication.shared.openURL(url)
+        }
+    }
+    func urlsubracas(_sender: AnyObject){
+        let url = URL(string: "http://dog.ceo/api/breed/hound/list")!
+        if UIApplication.shared.canOpenURL(url){
+        UIApplication.shared.openURL(url)
+    }
+  }
+    func urlimagem(_sender: AnyObject){
+        let url = URL(string: "http://images.dog.veo/breeds/whippet/n02091134_4273.jpg")!
+        if UIApplication.shared.canOpenURL(url){
+            UIApplication.shared.openURL(url)
+    }
+  }
+    func loadSampleRacas(){
+        switch listType{
+        case "Raças" :
+            racas += ["urltodasasracas"]
+            
+        case "subracas" :
+            racas += ["urlsubRacas"]
+       
+        case "Imagems":
+            racas += ["urlimagem"]
+        default:
+            print("Escolha uma opção existente!")
+     
+        }
+  }
 }
-
