eaa78a566421ebb23e7a701b699a015b9c218d20
João Eduardo Mendes Chaicoski <jemchaicoski@hotmail.com>
2020-03-06 08:49:47 -0300

    antes do merge

--- dogCEOIOS/TableViewCell.swift
+++ dogCEOIOS/TableViewCell.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class TableViewCell: UITableViewCell {
 
+    @IBOutlet weak var buttonFavorite: UIButton!
+    @IBOutlet weak var nomeRaca: UILabel!
+    @IBOutlet weak var imagemRaca: UIImageView!
+    
+    weak var delegate : TableViewCellDelegate?
+    
+    var raca: Dog?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        self.buttonFavorite.addTarget(self, action: #selector(getter: buttonFavorite), for: .touchUpInside)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -20,5 +30,20 @@ class TableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+    
+@IBAction func subscribeButtonTapped(_ sender: UIButton){
+    // ask the delegate (in most case, its the view controller) to
+    // call the function 'subscribeButtonTappedFor' on itself.
+    if let raca = raca, let delegate = delegate { self.delegate?.TableViewCell(self, subscribeButtonTappedFor: raca) }
+    raca!.favorito = !(raca!.favorito)
+    
+    }
 }
+
+ protocol TableViewCellDelegate: AnyObject { func TableViewCell(_ TableViewCell: TableViewCell, subscribeButtonTappedFor cell: Dog)
+    
+}
+
+
+
+
